72d21c2ca437254834d28f314f4d46825daff4e9
Vivian Yu <yuviv@Vivians-MacBook-Air.local>
2021-08-10 21:21:25 -0700

    update

--- grocerylist/src/grocerylist.js
+++ grocerylist/src/grocerylist.js
@@ -5,38 +5,37 @@ class List extends React.Component{
     constructor(props){
         super(props)
         this.state = {
-            list: ['shop', 'vacuum', 'exercise']
+            list: ['shop', 'vacuum', 'exercise'],
+            value: "",
         }
     }
     render(){
         const renderedList = this.state.list.map((entry) =>
-            <div className="listItem">{<input type="checkbox"></input>}{entry}</div>
+            <div className="listItem"><input type="checkbox" />{entry}</div>
         )
        const formSubmit = (e) =>{
-           let dup
+           const inputValue = e.target.value;
            if(e.key ==='Enter'){
-               for(let i=0; i < this.state.list.length; i++){
-                   if(this.state.list[i] == e.target.value){
-                       alert("This is a duplicate!")
-                       dup = true
-                       e.preventDefault()
-                       break;          
-                   }
-               }
-            //    I was having trouble with the break if a duplicate is found, 
-            // so I added the dup element to trigger true if a duplicate is found
-               if(dup !== true){
-                    this.setState({list: this.state.list.concat(e.target.value)})
-                    e.preventDefault();
+                e.preventDefault();
+                const dup = this.state.list.some(rowValue => rowValue === inputValue);
+                // I was having trouble with the break if a duplicate is found, 
+                // so I added the dup element to trigger true if a duplicate is found
+                if (dup) {
+                    alert("Duplicate item");
+                } else {
+                    this.setState({list: this.state.list.concat(inputValue), value: ""});
                 } 
            }
+        }
+        const onChange = (e) => {
+            this.setState({ value: e.target.value });
         }
         return(
             <div className="listContainer">
                 <h4>To Do List</h4>
                 {renderedList}
                 <form  className="listEntry">
-                    <input  className="entryForm" defaultValue="add item..." onKeyPress={formSubmit} type="text"></input>
+                    <input  className="entryForm" placeholder="add item..." value={this.state.value} onChange={onChange} onKeyPress={formSubmit} type="text" />
                 </form>
                 
             </div>
